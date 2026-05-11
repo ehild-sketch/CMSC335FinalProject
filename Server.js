3206bd54668b57686fd4ee3b3c7b6228860fd0bf
evabth <evanhild@verizon.net>
2026-05-11 16:40:44 -0400

    Added Pages folder and mongoose functionality

--- Server.js
+++ Server.js
@@ -1,6 +1,38 @@
-const express = require("express");
-const app = express();
 const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 const portNumber = 7003;
 const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(express.urlencoded({ extended: true }))
+require("dotenv").config({
+   path: path.resolve(__dirname, ".env"),
+   quiet:true
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "Pages"));
+
+
+
+app.get("/", (req, res) => { res.render("index")})
+
+
+
+async function main() {
+
+    try{
+        await mongoose.connect(process.env.MONGO_STRING);
+        console.log("Mongo DB connection successful")
+    }catch(e){
+        console.log(e)
+    }
+
+    app.listen(portNumber);
+
+    console.log(`Server Listening on Port:${portNumber}`)
+
+}
+
+
+main()
